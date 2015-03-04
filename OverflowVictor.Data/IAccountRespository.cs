abd4b5208d11cf7faa47db22536c0fde68a6020d
vicra <vicramirez_10@hotmail.com>
2015-03-04 00:36:12 -0600

    unit of work

---
+++ OverflowVictor.Data/IAccountRespository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using OverflowVictor.Domain.Entities;
+
+namespace OverflowVictor.Data
+{
+    public interface IAccountRespository : IDisposable
+    {
+        IEnumerable<Account> GetAccounts();
+        Account GetAccountById(Guid accountId);
+        void InsertAccount(Account account);
+        void DeleteAccount(Guid accountId);
+        void UpdateAccount(Account account);
+        void Save();
+    }
+}
---
+++ OverflowVictor.Data/IQuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using OverflowVictor.Domain.Entities;
+
+namespace OverflowVictor.Data
+{
+    public interface IQuestionRepository : IDisposable
+    {
+        IEnumerable<Question> GetQuestions();
+        Question GetQuestionById(Guid questionId);
+        void InsertQuestion(Question question);
+        void DeleteQuestion(Guid questionId);
+        void UpdateQuestion(Question question);
+        void Save();
+    }
+}
