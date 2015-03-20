f4b6133a7b41474f185aa3f98736e5d020234740
vicra <vicramirez_10@hotmail.com>
2015-03-19 22:27:33 -0600

    Removing Model Name

--- OverflowVictor.Web/Models/AccountRegisterModel.cs
+++ OverflowVictor.Web/Models/AccountRegisterModel.cs
@@ -6,11 +6,11 @@ namespace OverflowVictor.Web.Models
     public class AccountRegisterModel 
     {
         [Required]
-        [StringLength(50,ErrorMessage = "eroor",MinimumLength = 2)]
+        [StringLength(50, ErrorMessage = "The Name must be between 2 and 50 characters", MinimumLength = 2)]
         public string Name { get; set; }
 
         [Required]
-        [StringLength(50, ErrorMessage = "eroor", MinimumLength = 2)]
+        [StringLength(50, ErrorMessage = "The LastName must be between 2 and 50 characters", MinimumLength = 2)]
         public string LastName { get; set; }
 
 
@@ -20,12 +20,12 @@ namespace OverflowVictor.Web.Models
 
         [Required]
         [DataType(DataType.Password)]
-        [StringLength(16, ErrorMessage = "eroor", MinimumLength = 8)]
+        [StringLength(16, ErrorMessage = "The password must be between 8 and 16 characters", MinimumLength = 8)]
         [Vocal]
         [Number]
         [RepeatedLetters]
         [LettersAndNumber]
-        [Capital(ErrorMessage = "Must contain a capital letter")]
+        [Capital(ErrorMessage = "The password must be between 8 and 16 characters")]
         public string Password { get; set; }
 
         [Required]
